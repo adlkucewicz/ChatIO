1f5d3ab23c9652c298daf79b4e399910ca89c2e0
RichardInnocent <richardinnocent@sky.com>
2021-02-11 22:40:48 +0000

    Removed unncessary print statement

--- chatio_api/chatio_api/views.py
+++ chatio_api/chatio_api/views.py
@@ -5,6 +5,5 @@ import json
 
 # Create your views here.
 def status(request):
-    print('Hello')
     response = json.dumps({'status': 'RUNNING'})
     return HttpResponse(response, content_type='application/json')
